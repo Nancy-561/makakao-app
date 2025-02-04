419c56dcdd0a9338929a5f274b1cb1e1970a463d
anmol-55 <anmolsinghasdf55@gmail.com>
2025-02-04 17:56:55 +0530

    signup screen

---
+++ src/Mocks/productMock.js
@@ -0,0 +1,118 @@
+export const AvailableProductCategories = [{
+    "name": "clothing",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 6
+},
+{
+    "name": "Eyewear",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 7
+},
+{
+    "name": "Watches",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 5
+},
+{
+    "name": "Jewelry",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 2
+},
+{
+    "name": "Accesories",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 0
+},
+{
+    "name": "Furniture",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 8
+},
+{
+    "name": "Beauty",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 5
+},
+{
+    "name": "Gifts",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "count": 0
+}
+]
+
+export const products = [{
+    "categrory": "watch",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 2499.00,
+    "liveBidding": true,
+    "bidPrice": 10,
+    "discount": 0
+},
+{
+    "categrory": "watch",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 2499.00,
+    "liveBidding": true,
+    "bidPrice": 9,
+    "discount": 0
+},
+{
+    "categrory": "watch",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 2499.00,
+    "liveBidding": true,
+    "bidPrice": 10,
+    "discount": 0
+},
+{
+    "categrory": "watch",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 2499.00,
+    "liveBidding": true,
+    "bidPrice": 7,
+    "discount": 0
+},
+{
+    "categrory": "watch",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 2499.00,
+    "liveBidding": true,
+    "bidPrice": 8,
+    "discount": 0
+},
+{
+    "categrory": "sunglasses",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 1235.00,
+    "liveBidding": false,
+    "discount": 23
+},
+{
+    "categrory": "sunglasses",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 1256.00,
+    "liveBidding": false,
+    "discount": 26
+},
+{
+    "categrory": "sunglasses",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 7890.00,
+    "liveBidding": false,
+    "discount": 23
+},
+{
+    "categrory": "sunglasses",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 2345.54,
+    "liveBidding": false,
+    "discount": 23
+},
+{
+    "categrory": "watch",
+    "imageUrl": "https://static.vecteezy.com/system/resources/thumbnails/029/282/385/small_2x/pixel-art-sweater-pixelated-sweater-clothes-sweater-autumn-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector.jpg",
+    "price": 1899.00,
+    "liveBidding": false,
+    "discount": 23
+},
+]
--- src/Screens/SignUp.jsx
+++ src/Screens/SignUp.jsx
@@ -1,10 +1,48 @@
 import { AuthLayout } from "../Layout/AuthLayout";
 
 export const SignUp = ({ children }) => {
-    return (
-      <AuthLayout>
-        <div>Sign Up Page</div>
-      </AuthLayout>
-    );
-  }
-  
+  return (
+    <AuthLayout>
+      <div className="bg-[#F9F8F3] flex flex-col">
+        <div className="bg-[#FFFFFF] pt-[10px] pb-[10px] rounded-[16px] w-[362px] m-auto">
+          <div className="w-[90%] m-auto flex justify-around flex-col gap-[25px]">
+            <div className="text-left">
+              <div className="font-black text-[25.6px]">Create an account</div>
+              <div className="font-semibold text-[14.4px]"><span className="text-[#A7A8BB]">Already have an account?</span> <a>Login</a></div>
+            </div>
+            <div className="text-left flex flex-col gap-[4px]">
+              <div>
+                <div>Name</div>
+                <input type="text" id="name" className="bg-[#EEF1F5] rounded-[9.6px] w-[100%] h-[56px]" />
+              </div>
+              <div>
+                <div>Email</div>
+                <input type="email" id="email" className="bg-[#EEF1F5] rounded-[9.6px] w-[100%] h-[56px]" />
+              </div>
+              <div>
+                <div>Password</div>
+                <input type="password" id="password" className="bg-[#EEF1F5] rounded-[9.6px] w-[100%] h-[56px]" />
+              </div>
+              <div>
+                <div>Confirm Password</div>
+                <input type="password" id="confirm-password" className="bg-[#EEF1F5] rounded-[9.6px] w-[100%] h-[56px]" />
+              </div>
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <div className="bg-black text-white rounded-[4.8px] pt-[5px] h-[30px] pb-[30px]"><button>Sign up</button></div>
+              <div className="bg-[#E1F0FF] rounded-[4.8px] pt-[5px] h-[30px] pb-[30px]"><button >Sign in with Google</button></div>
+            </div>
+          </div>
+        </div>
+        <div className="border-2 border-black flex gap-[10px]">
+          <div>Terms</div>
+          <div>Plans</div>
+          <div>Contact Us</div>
+        </div>
+      </div>
+
+
+
+    </AuthLayout>
+  );
+}
